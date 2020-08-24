93dc08ebda6411b6c76ba0248fef69a749332efd
Andrii Loburets <loburets.andrii@gmail.com>
2020-08-24 22:13:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,109 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+// 1. FILES
+// Blocking synchronous way
+// const textIn = fs.readFileSync("./starter/txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on ${Date.now()}`;
+// // fs.writeFileSync("./starter/txt/output.txt", textOut);
+
+// // Non-blocking asynchronous way
+// fs.readFile("./starter/txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./starter/txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log("data 2 has been read");
+//     fs.readFile("./starter/txt/append.txt", "utf-8", (err, data3) => {
+//       console.log("data 3 has been read");
+//       fs.writeFile(
+//         "./starter/txt/output-async.txt",
+//         `${data2}\n${data3}`,
+//         "utf-8",
+//         (err) => {
+//           console.log("Your file has been written :-)");
+//         }
+//       );
+//     });
+//   });
+// });
+
+// console.log("reading file...");
+
+// 2.SERVER
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/starter/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/starter/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/starter/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(
+  `${__dirname}/starter/dev-data/data.json`,
+  "utf-8"
+);
+const dataObject = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
+
+  // OVERVIEW PAGE
+
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObject
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+
+    // PRODUCT PAGE
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObject[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    //API
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+    // console.log(dataObject);
+
+    // NOT FOUND
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello world",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
